6dc384fb032d3cd05e4cfc2df354e6054404fd08
itz-sarthak-2003 <128714788+itz-sarthak-2003@users.noreply.github.com>
2024-03-24 15:27:45 +0530

    Add files via upload

---
+++ ServerSideData.java
@@ -0,0 +1,26 @@
+import java.net.*;
+import java.io.*;
+public class ServerSideData
+{
+    public static void main(String[] args) throws IOException {
+        DatagramSocket ds = new DatagramSocket(2019);
+        byte[] receiveData = new byte[512];
+        byte[] sendData = new byte[512]; 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(" UDP Server Socket is created, waiting for client "); 
+        do
+        {
+            DatagramPacket receiveDP = new DatagramPacket(receiveData,receiveData.length);
+            ds.receive(receiveDP); 
+            String clientMessage = new String(receiveDP.getData(),0,receiveDP.getLength());
+            System.out.println("Client Message:"+clientMessage);
+            InetAddress ip = receiveDP.getAddress();
+            System.out.print("\n\nEnter Server Message:");
+            String serverMessage = br.readLine();
+            sendData = serverMessage.getBytes();
+            DatagramPacket sendDP = new DatagramPacket(sendData, sendData.length, ip, receiveDP.getPort()); 
+            ds.send(sendDP);             
+            receiveData = new byte[512];
+        }while(true);
+        }
+        }
